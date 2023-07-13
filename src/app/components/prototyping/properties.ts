679d7c7ae0017e3ec6244366a55271a788b34efe
Thales <thalesmoreirax@gmail.com>
2023-07-13 11:37:23 -0300

    agora que costrui a estrutura generalizei a aplicação

---
+++ src/app/components/prototyping/properties.ts
@@ -0,0 +1,38 @@
+export const Propreties = {
+  linesVertical: {
+    array: ['v'],
+    buttonType: 'linesVertical',
+  },
+  linesHorizontal: {
+    array: ['h'],
+    buttonType: 'linesHorizontal',
+  },
+  genericLoad: {
+    array: ['g'],
+    buttonType: 'genericLoad',
+  },
+  key: {
+    array: ['k'],
+    buttonType: 'key',
+  },
+  transformers: {
+    array: ['t'],
+    buttonType: 'transformers',
+  },
+  exitPoint: {
+    array: ['e'],
+    buttonType: 'exitPoint',
+  },
+  entryPoint: {
+    array: ['p'],
+    buttonType: 'entryPoint',
+  },
+  circuitBreaker: {
+    array: ['c'],
+    buttonType: 'circuitBreaker',
+  },
+  feeder: {
+    array: ['f'],
+    buttonType: 'feeder',
+  },
+};
--- src/app/components/prototyping/prototyping.component.ts
+++ src/app/components/prototyping/prototyping.component.ts
@@ -1,94 +1,44 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SelectComponent } from '../modal/select/select.component';
+import { Propreties } from './properties';
 @Component({
   selector: 'app-prototyping',
   templateUrl: './prototyping.component.html',
   styleUrls: ['./prototyping.component.css'],
 })
 export class PrototypingComponent {
-  linesVertical: any[] = ['v'];
-  linesHorizontal: any[] = ['h'];
-  genericLoad: any[] = ['g'];
-  keys: any[] = ['k'];
-  transformers : any[] = ['t'];
-  exitPoints : any[] = ['e'];
-  entryPoints : any[] = ['p'];
-  circuitBreakers : any[] = ['c'];
-  feeders : any[] = ['f'];
+  public propertiesData: any = Propreties;
 
   id: string = '0';
 
-  
   constructor(public dialog: MatDialog) {
+    console.log(this.propertiesData);
   }
 
-  openDialog(){
-    const config = new MatDialogConfig()
+  openDialog() {
+    const config = new MatDialogConfig();
     config.height = 'auto';
     config.width = 'auto';
-    
+
     const dialog = this.dialog.open(SelectComponent, config);
-
   }
-
-
-
-
-
 
   handleDragStart(event: DragEvent, id: string) {
     event.dataTransfer?.setData('text/plain', '');
     event.dataTransfer?.setDragImage(event.target as HTMLElement, 0, 0);
     this.id = id;
+    console.log(id)
   }
   addLines(type: string) {
-    if (type == 'linesVerdical') {
-      let index = this.linesVertical.length.toString();
-      this.linesVertical.push(this.linesVertical[0] + index);
-      this.displayButton('none', 'verdical');
+    if (this.propertiesData.hasOwnProperty(type)) {
+      const option = this.propertiesData[type];
+      let index = option.array.length.toString();
+      option.array.push(option.array[0] + index);
+      this.displayButton('none', option.buttonType);
+    } else {
+      console.log('Objeto não encontrado');
     }
-    if (type == 'linesHorizontal') {
-      let index = this.linesHorizontal.length.toString();
-      this.linesHorizontal.push(this.linesHorizontal[0] + index);
-      this.displayButton('none', 'horizontal');
-    }
-    if (type == 'genericLoad') {
-      let index = this.genericLoad.length.toString();
-      this.genericLoad.push(this.genericLoad[0] + index);
-      this.displayButton('none', 'genericLoad');
-    }
-    if (type == 'key') {
-      let index = this.keys.length.toString();
-      this.keys.push(this.keys[0] + index);
-      this.displayButton('none', 'key');
-    }
-    if (type == 'transformer') {
-      let index = this.transformers.length.toString();
-      this.transformers.push(this.transformers[0] + index);
-      this.displayButton('none', 'transformer');
-    }
-    if (type == 'exitPoint') {
-      let index = this.exitPoints.length.toString();
-      this.exitPoints.push(this.exitPoints[0] + index);
-      this.displayButton('none', 'exitPoint');
-    }
-    if (type == 'entryPoint') {
-      let index = this.entryPoints.length.toString();
-      this.entryPoints.push(this.entryPoints[0] + index);
-      this.displayButton('none', 'entryPoint');
-    }
-    if (type == 'circuitBreaker') {
-      let index = this.circuitBreakers.length.toString();
-      this.circuitBreakers.push(this.circuitBreakers[0] + index);
-      this.displayButton('none', 'circuitBreaker');
-    }
-    if (type == 'feeder') {
-      let index = this.feeders.length.toString();
-      this.feeders.push(this.feeders[0] + index);
-      this.displayButton('none', 'feeder');
-    }
-    console.log(this.linesHorizontal, this.linesVertical, this.genericLoad, this.keys,this.transformers)
   }
 
   displayButton(display: string, id: string) {
@@ -111,21 +61,22 @@ export class PrototypingComponent {
       linha.style.left = offsetX + 'px';
       linha.style.top = offsetY + 'px';
 
-      if (this.id[0] == 'v') this.displayButton('block', 'verdical');
-      if (this.id[0] == 'h') this.displayButton('block', 'horizontal');
-      if (this.id[0] == 'g') this.displayButton('block', 'genericLoad');
-      if (this.id[0] == 'k') this.displayButton('block', 'key');
-      if (this.id[0] == 't') this.displayButton('block', 'transformer');
-      if (this.id[0] == 'e') this.displayButton('block', 'exitPoint');
-      if (this.id[0] == 'p') this.displayButton('block', 'entryPoint');
-      if (this.id[0] == 'c') this.displayButton('block', 'circuitBreaker');
-      if (this.id[0] == 'f') this.displayButton('block', 'feeder');
-      this.openDialog();
-
-
-
-
-
+      for(let prop in this.propertiesData) {
+        if(this.id[0] == this.propertiesData[prop].array[0]){
+          console.log(prop)
+          this.displayButton('block',this.propertiesData[prop].buttonType)
+        }
+      }
+      // if (this.id[0] == 'v') this.displayButton('block', 'linesVerdical');
+      // if (this.id[0] == 'h') this.displayButton('block', 'linesHorizontal');
+      // if (this.id[0] == 'g') this.displayButton('block', 'genericLoad');
+      // if (this.id[0] == 'k') this.displayButton('block', 'key');
+      // if (this.id[0] == 't') this.displayButton('block', 'transformer');
+      // if (this.id[0] == 'e') this.displayButton('block', 'exitPoint');
+      // if (this.id[0] == 'p') this.displayButton('block', 'entryPoint');
+      // if (this.id[0] == 'c') this.displayButton('block', 'circuitBreaker');
+      // if (this.id[0] == 'f') this.displayButton('block', 'feeder');
+      // this.openDialog();
     }
   }
 }
@@ -170,60 +121,60 @@ export class PrototypingComponent {
 //     console.log(X, Y)
 //   // console.log(`clique nas coordernadas ${x},${y}`);
 // }
-// 
-// 
+//
+//
 // interface Node {
-  // value: string;
-  // children: Node[];
+// value: string;
+// children: Node[];
 // }
-// 
+//
 // function searchObject(node: Node, targetValue: string): boolean {
-  // if (node.value === targetValue) {
-    // return true;
-  // }
-// 
-  // for (const child of node.children) {
-    // if (searchObject(child, targetValue)) {
-      // return true;
-    // }
-  // }
-// 
-  // return false;
+// if (node.value === targetValue) {
+// return true;
 // }
-// 
+//
+// for (const child of node.children) {
+// if (searchObject(child, targetValue)) {
+// return true;
+// }
+// }
+//
+// return false;
+// }
+//
 // Exemplo de uso
 // const obj: Node = {
-  // value: "A",
-  // children: [
-    // {
-      // value: "B",
-      // children: [
-        // {
-          // value: "D",
-          // children: [],
-        // },
-        // {
-          // value: "E",
-          // children: [],
-        // },
-      // ],
-    // },
-    // {
-      // value: "C",
-      // children: [
-        // {
-          // value: "F",
-          // children: [],
-        // },
-        // {
-          // value: "G",
-          // children: [],
-        // },
-      // ],
-    // },
-  // ],
+// value: "A",
+// children: [
+// {
+// value: "B",
+// children: [
+// {
+// value: "D",
+// children: [],
+// },
+// {
+// value: "E",
+// children: [],
+// },
+// ],
+// },
+// {
+// value: "C",
+// children: [
+// {
+// value: "F",
+// children: [],
+// },
+// {
+// value: "G",
+// children: [],
+// },
+// ],
+// },
+// ],
 // };
-// 
+//
 // console.log(searchObject(obj, "D")); // true
 // console.log(searchObject(obj, "G")); // true
 // console.log(searchObject(obj, "H")); // false
